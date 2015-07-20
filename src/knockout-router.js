03b10322d356999810a7ba18d2ffc3a822ab42c1
Christian Michael Paul Bankester <cbankester@immense.net>
2015-07-20 10:22:50 -0500

    Added eslint, immense eslint config

--- src/knockout-router.js
+++ src/knockout-router.js
@@ -1,63 +1,57 @@
+import assert from 'assert';
 import RouteRecognizer from 'route-recognizer';
 
 export default class Router {
-  _configureComponent(component_vm) {
-    let {name, template} = component_vm;
+  getControllerForRoute({controller, handler}) {
+    let ret = controller;
+    if (!ret) ret = typeof handler === 'string' ? this[handler] || this.app[handler] : this;
+    return ret;
+  }
+  configureComponent(component_vm) {
+    const {name, template} = component_vm;
     console.log({name, template});
-    if (!name) throw new Error(`Cannot register component without a name; please define static method ${component_vm.constructor.name}.name`);
-    if (!template) throw new Error(`Cannot register component without template; please define static method ${component_vm.constructor.name}.template`);
+    assert(name, `Cannot register component without a name; please define static method ${component_vm.constructor.name}.name`);
+    assert(template, `Cannot register component without template; please define static method ${component_vm.constructor.name}.template`);
     ko.components.register(name, {
       viewModel: component_vm,
-      template: template
+      template
     });
   }
-  _generateHandler(route) {
-    let {path, handler, meta, controller, page} = route;
-    if ('string' !== typeof handler && 'function' !== typeof handler) {
-      throw new Error('`handler` should be either string or function');
-    }
-    if (!controller) {
-      if ('string' === typeof handler) {
-        if (this[handler]) controller = this;
-        else if (this.app[handler]) controller = app;
-        else throw new Error(`Cannot find method ${handler} on the router or on the the main view model`);
-
-      } else {
-        // Just assume that the desired controller is the router itself (i.e. `this`)
-        controller = this;
-      }
-    }
+  generateHandler(route) {
+    const {path, handler, page} = route;
+    assert(typeof handler === 'string' || typeof handler === 'function', '`handler` should be either string or function');
+    const controller = this.getControllerForRoute(route);
+    assert(controller, `Cannot find method ${handler} on the router or on the the main view model`);
+    let {meta} = route;
     if (!meta) meta = {};
     return params => {
-      let attenuated_params = Object.assign(meta, params);
+      const attenuated_params = Object.assign(meta, params);
       return Promise.resolve()
       .then(() => this.preHandle(path, handler, attenuated_params))
-      .then(() => ('string' === typeof handler ? controller[handler].bind(controller) : handler.bind(controller))(attenuated_params))
+      .then(() => (typeof handler === 'string' ? controller[handler].bind(controller) : handler.bind(controller))(attenuated_params))
       .then(component_params => {
         if (page) {
           this.current_page(page.name);
-          this.current_params(attenuated_params);
+          this.current_params(component_params);
         } else {
           this.current_page(void 0);
           this.current_params(void 0);
         }
       })
       .then(() => this.postHandle(path, handler, attenuated_params));
-    }
+    };
   }
   constructor(app, routes) {
-    this.app              = app;
-    this.routes           = routes;
+    this.app = app;
+    this.routes = routes;
     this.route_recognizer = new RouteRecognizer();
-    this.current_page     = ko.observable();
-    this.current_params   = ko.observable();
-    for (let route of routes) {
-      if (route.page) {
-        this._configureComponent(route.page);
-      }
+    this.current_page = ko.observable();
+    this.current_params = ko.observable();
+    for (const route of routes) {
+      if (route.page) this.configureComponent(route.page);
       this.route_recognizer.add([{
-        path:    route.path,
-        handler: this._generateHandler(route)
+        path: route.path,
+        handler: this.generateHandler(route)
       }]);
     }
   }
@@ -74,23 +68,23 @@ export default class Router {
     });
   }
   unrecognizedRouteHandler(path) {
-    let err = new Error("404 Not Found");
+    const err = new Error("404 Not Found");
     err.path = path;
     err.status = 404;
     return Promise.reject(err);
   }
 
   handlePath(path, opts={}) {
-    let recognized_route = this.route_recognizer.recognize(path);
+    const recognized_route = this.route_recognizer.recognize(path);
     if (!recognized_route) return this.unrecognizedRouteHandler(path);
-    let {handler, params} = recognized_route[0];
-    let attenuated_params = Object.assign(params, opts);
+    const {handler, params} = recognized_route[0];
+    const attenuated_params = Object.assign(params, opts);
     return handler(attenuated_params)
-    .then(() => Promise.resolve(attenuated_params
+    .then(() => Promise.resolve(attenuated_params))
     .catch(err => {
-      let e = new Error(`Could not handle path '${path}': ${err.message || err}`);
+      const e = new Error(`Could not handle path '${path}': ${err.message || err}`);
       e.original_error = err;
-      e.params         = attenuated_params;
+      e.params = attenuated_params;
       return Promise.reject(e);
     });
   }
